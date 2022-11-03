b998d766c56c92682a75d5e70e6bce49edc39451
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2022-11-03 09:46:34 -0600

    Initial Commit

---
+++ AsyncThrowImage/AsyncThrowImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncThrowImageApp.swift
+//  AsyncThrowImage
+//
+//  Created by Marco Alonso Rodriguez on 03/11/22.
+//
+
+import SwiftUI
+
+@main
+struct AsyncThrowImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
